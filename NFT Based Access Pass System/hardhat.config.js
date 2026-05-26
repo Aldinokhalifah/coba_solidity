6cc31a416db4334c7dfa4bc76f74ab7e59785979
Aldinokhalifah <khalifahaldino@gmail.com>
2026-05-26 10:36:07 +0700

    Menambahkan jaringan megaeth tesnet dan deployed kontrak

--- NFT Based Access Pass System/hardhat.config.js
+++ NFT Based Access Pass System/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,4 +9,12 @@ module.exports = {
       evmVersion: "cancun",
     },
   },
+  networks: {
+        hardhat: {},
+
+        megaeth: {
+            url: process.env.MEGAETH_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
 };
